751cd8373c707e86ad10843ad4896163d4dcf07e
Agustin Gomez <agusleongomez@gmail.com>
2022-04-25 17:36:49 -0300

    BaSP-W06-2022: login validation ended, started the emergent sign

---
+++ Semana-06/javascript/login.js
@@ -0,0 +1,95 @@
+window.onload = function () {
+    //Values from document
+    var email = document.getElementById('email');
+    var span = document.getElementsByClassName('error');
+    var password = document.getElementById('password');
+    var button = document.getElementsByClassName('btn-send')
+    var sign = document.getElementById('info-sign');
+    var title = document.getElementById('message');
+    var emailMessage = document.getElementById('email-message');
+    var passwordMessage = document.getElementById('password-message');
+
+    //Email validation
+    email.addEventListener('blur', emailBlur);
+    email.addEventListener('focus', emailFocus);
+
+    function emailBlur(){
+        var validEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+        if(email.value === ''){
+            span[0].textContent = 'Please enter a email';
+        } else if(!validEmail.test(email.value)){
+            span[0].textContent = 'Please enter a valid email';
+        } else{
+            return true;
+        }
+    }
+
+    function emailFocus(){
+        span[0].textContent = '';
+    }
+
+    //Password Validation
+    password.addEventListener('blur', passwordBlur);
+    password.addEventListener('focus', passwordFocus);
+
+    function passwordBlur(){
+        var alphabet = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        if(password.value.length == 0){
+            span[1].textContent = 'Please enter a password';
+        } else if(password.value.length < 6){
+            span[1].textContent = 'Password is too short'; //si son menos de 6 tira error
+        } else{
+            var contLetters = 0;
+            var contNumbers = 0;
+            var flag = 0; //bandera va a mostrar si hay un valor que no este dentro del arreglo
+            for(var i = 0; i < password.value.length; i++){
+                for(var j = 0; j < alphabet.length; j++){
+                    if(password.value.charAt(i) == alphabet[j]){
+                        contLetters += 1;
+                    }
+                }
+                for(var j = 0; j < numbers.length; j++){
+                    if(password.value.charAt(i) == numbers[j]){
+                        contNumbers += 1;
+                    }
+                }
+            }
+            if(contLetters == 0 || contNumbers == 0){
+                span[1].textContent = 'Password needs numbers and letters';
+            } else if(contLetters + contNumbers !== password.value.length){
+                span[1].textContent = 'Password only accept numbers and letters';
+            } else{
+                return true;
+            }
+        }
+    }
+
+    function passwordFocus(){
+        span[1].textContent = '';
+    }
+
+    //Cartel emergente
+    button.addEventListener('click', showSign);
+
+    function showSign(event){
+        event.preventDefault();
+        if(emailBlur == true && passwordBlur == true){
+            sign.classList('shown');
+            title.textContent = 'Login successfull';
+            emailMessage.textContent = 'Email:' + email;
+            passwordMessage.textContent = 'Password:' + password;
+        } else{
+            sign.classList('shown');
+            title.textContent = 'Error';
+            if(emailBlur != true){
+                emailMessage.textContent = 'Email:' + email + '(Invalid Email)';
+            } else{
+                emailMessage.textContent = 'Email:' + email;
+            }
+            if(passwordBlur != true){
+                passwordMessage.textContent = 'Password:' + password + '(Invalid Password)';
+            }
+        }
+    }
+}
